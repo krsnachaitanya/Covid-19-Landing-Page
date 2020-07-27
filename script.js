714203f491600cbe85f469b8adfc78cdd0aedfdc
Krishna Chaitanya <chaitu.ca539@gmail.com>
2020-07-27 17:13:43 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,12 @@
+(function($) { 
+  $(function() { 
+    // Toggle open and close nav styles on click
+    $('#nav-toggle').click(function() {
+      $('nav ul').slideToggle();
+    });
+    // Hamburger to X toggle
+    $('#nav-toggle').on('click', function() {
+      this.classList.toggle('active');
+    });
+  }); 
+})(jQuery); 
